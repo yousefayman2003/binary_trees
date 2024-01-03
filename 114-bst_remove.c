4a2bbcec83ce5b689eb1605dadeb8e268d9801fb
yousefayman <yousefayman20003@gmail.com>
2024-01-03 10:22:17 -0800

    fixed betty

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -6,13 +6,13 @@
  * @value: value to remove in the tree
  *
  * Return: pointer to the new root node of the tree after removing
- * 	the desired value
+ *		the desired value
 */
 bst_t *bst_remove(bst_t *root, int value)
 {
 	if (root == NULL)
 		return (NULL);
-	
+
 	if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
@@ -23,9 +23,10 @@ bst_t *bst_remove(bst_t *root, int value)
 			return (root->right);
 		else if (!root->right)
 			return (root->left);
-		
+
 		/* finding min value from right sub-tree */
 		bst_t *curr = root->right;
+
 		while (curr->left)
 			curr = curr->left;
 		root->n = curr->n;
