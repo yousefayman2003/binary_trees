a380b92aacbd83d5ac78bbcb57275dbdff85ecaa
yousefayman <yousefayman20003@gmail.com>
2024-01-02 16:21:24 -0800

    fixed betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,9 +5,11 @@
  * @first: pointer to the first node
  * @second: pointer to the second node
  *
- * Return: pointer to the lowest common ancestor node of the two given nodes
+ * Return: pointer to the lowest common ancestor node of
+ * the two given nodes
 */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	const binary_tree_t *first_anc = first, *second_anc;
 
