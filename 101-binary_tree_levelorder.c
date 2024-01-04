5e8ec89dbacb2ad6bec7ab19f1e5cd49ab0abb43
yousefayman <yousefayman20003@gmail.com>
2024-01-04 08:41:34 -0800

    test

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size_t - measures the size of a binary tree
+ * binary_tree_size_te - measures the size of a binary tree
  * @tree: pointer to the root node of the tree to measure the size
  *
  * Return: size of tree
 */
-size_t binary_tree_size_t(const binary_tree_t *tree)
+size_t binary_tree_size_te(const binary_tree_t *tree)
 {
 	size_t n_nodes = 1;
 
@@ -14,8 +14,8 @@ size_t binary_tree_size_t(const binary_tree_t *tree)
 		return (0);
 
 	/* recursively add a node */
-	n_nodes += binary_tree_size_t(tree->left);
-	n_nodes += binary_tree_size_t(tree->right);
+	n_nodes += binary_tree_size_te(tree->left);
+	n_nodes += binary_tree_size_te(tree->right);
 
 	return (n_nodes);
 }
@@ -36,7 +36,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	/* get size of tree */
-	size = binary_tree_size_t(tree);
+	size = binary_tree_size_te(tree);
 	/* make a queue */
 	const binary_tree_t **queue = malloc(sizeof(const binary_tree_t *) * size);
 	/* enqueue the root */
